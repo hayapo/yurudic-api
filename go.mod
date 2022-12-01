8700121e4752e435c1c5737786e5076df747261f
hayapo <keep.forward1001@gmail.com>
2022-12-02 02:02:05 +0900

    :zap: Start Project

---
+++ go.mod
@@ -0,0 +1,5 @@
+module yurudic-api
+
+go 1.19
+
+//require github.com/elastic/go-elasticsearch/v7 v7.17.7
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	// es7 "github.com/elastic/go-elasticsearch/v7"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+}
